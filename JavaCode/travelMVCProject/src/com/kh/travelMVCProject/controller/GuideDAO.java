a6af825b946653fcadbeb022c3dc8f91ffbd4292
Jiyeon Jeon <jiyeonjeon01@naver.com>
2024-12-02 02:48:24 +0900

    [update] travelMVCProject 파일 작동까지 완료

---
+++ JavaCode/travelMVCProject/src/com/kh/travelMVCProject/controller/GuideDAO.java
@@ -0,0 +1,138 @@
+package com.kh.travelMVCProject.controller;
+
+import java.util.ArrayList;
+
+import com.kh.travelMVCProject.model.GuideVO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.kh.travelMVCProject.model.GuideVO;
+
+public class GuideDAO {
+	public final String GUIDE_INSERT = "INSERT INTO GUIDE VALUES(GUIDE_SEQ.NEXTVAL, ?, ?, ?, ?)";
+	public final String GUIDE_SELECT = "SELECT * FROM GUIDE";
+	public final String GUIDE_DELETE = "DELETE FROM GUIDE WHERE NO = ?";
+	public final String GUIDE_UPDATE = "UPDATE GUIDE SET NAME = ?, PHONE = ?, LANGUAGES = ? WHERE NO = ?";
+	public final String GUIDE_SORT = "SELECT * FROM GUIDE ORDER BY ID";
+
+	// insert
+	public boolean guideInsert(GuideVO gvo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		boolean successFlag = false;
+		try {
+			con = DBUtility.dbCon();
+			pstmt = con.prepareStatement(GUIDE_INSERT);
+			pstmt.setString(1, gvo.getId());
+			pstmt.setString(2, gvo.getName());
+			pstmt.setString(3, gvo.getPhone());
+			pstmt.setString(4, gvo.getLanguages());
+			int result = pstmt.executeUpdate();
+			successFlag = (result != 0) ? true : false;
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		} finally {
+			DBUtility.dbClose(con, pstmt);
+		}
+		return successFlag;
+	}
+
+	// select
+	public ArrayList<GuideVO> guideSelect(GuideVO gvo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<GuideVO> guideList = new ArrayList<GuideVO>();
+		try {
+			con = DBUtility.dbCon();
+			pstmt = con.prepareStatement(GUIDE_SELECT);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				int no = rs.getInt("NO");
+				String id = rs.getString("ID");
+				String name = rs.getString("NAME");
+				String phone = rs.getString("PHONE");
+				String languages = rs.getString("LANGUAGES");
+				GuideVO guideVO = new GuideVO(no, id, name, phone, languages);
+				guideList.add(guideVO);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		} finally {
+			DBUtility.dbClose(con, pstmt, rs);
+		}
+		return guideList;
+	}
+
+	// delete
+	public boolean guideDelete(GuideVO gvo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		boolean successFlag = false;
+		try {
+			con = DBUtility.dbCon();
+			pstmt = con.prepareStatement(GUIDE_DELETE);
+			pstmt.setInt(1, gvo.getNo());
+			int result = pstmt.executeUpdate();
+			successFlag = (result != 0) ? true : false;
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		} finally {
+			DBUtility.dbClose(con, pstmt);
+		}
+		return successFlag;
+	}
+
+	// update
+	public boolean guideUpdate(GuideVO gvo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		boolean successFlag = false;
+		try {
+			con = DBUtility.dbCon();
+			pstmt = con.prepareStatement(GUIDE_UPDATE);
+			pstmt.setString(1, gvo.getName());
+			pstmt.setString(2, gvo.getPhone());
+			pstmt.setString(3, gvo.getLanguages());
+			pstmt.setInt(4, gvo.getNo());
+			int result = pstmt.executeUpdate();
+			successFlag = (result != 0) ? true : false;
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		} finally {
+			DBUtility.dbClose(con, pstmt);
+		}
+		return successFlag;
+	}
+
+	// sort
+	public ArrayList<GuideVO> guideSort(GuideVO gvo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<GuideVO> guideList = new ArrayList<GuideVO>();
+		try {
+			con = DBUtility.dbCon();
+			pstmt = con.prepareStatement(GUIDE_SORT);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				int no = rs.getInt("NO");
+				String id = rs.getString("ID");
+				String name = rs.getString("NAME");
+				String phone = rs.getString("PHONE");
+				String languages = rs.getString("LANGUAGES");
+				GuideVO guideVO = new GuideVO(no, id, name, phone, languages);
+				guideList.add(guideVO);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		} finally {
+			DBUtility.dbClose(con, pstmt, rs);
+		}
+		return guideList;
+	}
+}
--- JavaCode/travelMVCProject/src/com/kh/travelMVCProject/controller/PackageDAO.java
+++ JavaCode/travelMVCProject/src/com/kh/travelMVCProject/controller/PackageDAO.java
@@ -1,18 +1,22 @@
 package com.kh.travelMVCProject.controller;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
 import com.kh.travelMVCProject.model.PackageVO;
+import com.kh.travelMVCProject.model.ReviewVO;
 
 //private int no;
 //private String id;
 //private String name;
 //private int pCapacity;
 //private String national;
+//int price
 //private String guideId;
 //private Date sDate;
 //private Date eDate;
@@ -20,6 +24,13 @@ import com.kh.travelMVCProject.model.PackageVO;
 public class PackageDAO {
 
 	// query 
+	public final String PACKAGE_SELECT = "SELECT * FROM PACKAGE";
+	public final String PACKAGE_INSERT = 
+		    "INSERT INTO PACKAGE (NO, ID, NAME, PCAPACITY, NATIONAL, PRICE, GUIDE_ID, SDATE, EDATE) " +
+		    "VALUES (PACKAGE_SEQ.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?)";
+	public final static String PACKAGE_UPDATE = "UPDATE PACKAGE SET NAME = ?, PCAPACITY = ?, NATIONAL = ? , PRICE = ?, SDATE = ?, EDATE = ? WHERE PACKAGE_ID = ?";
+	public final String PACKAGE_DELETE = "DELETE FROM PACKAGE WHERE NO = ?";
+	public final String PACKAGE_SORT = "SELECT * FROM PACKAGE ORDER BY PRICE DESC";
 	
 	
 	
@@ -31,7 +42,36 @@ public class PackageDAO {
 		
 		ArrayList<PackageVO> packageList = new ArrayList<PackageVO>();
 		
-		// 내용 
+		try {
+			con = DBUtility.dbCon();
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(PACKAGE_SELECT);
+			
+			if(rs.next()) {
+				do {
+					int no = rs.getInt("NO");
+					String id = rs.getString("ID");
+					String name = rs.getString("NAME");
+					int pCapacity = rs.getInt("PCAPACITY");
+					String national = rs.getString("NATIONAL");
+					int price = rs.getInt("PRICE");
+					String guideId = rs.getString("GUIDE_ID");
+					Date sDate = rs.getDate("SDATE");
+					Date eDate = rs.getDate("EDATE");
+					
+					PackageVO pvo = new PackageVO(no, id, name, pCapacity, national, price, guideId, sDate, eDate);
+					packageList.add(pvo);
+				} while(rs.next());
+			}else {
+				
+			packageList = null;
+				
+			}
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		} finally {
+			DBUtility.dbClose(con, stmt, rs);
+		}
 		
 		return packageList;
 	}
@@ -39,17 +79,96 @@ public class PackageDAO {
 	
 	
 	
-	 // insert 
-	public boolean packageInsert(PackageVO pvo) {
-		boolean successFlag = false; // 성공여부
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		
-		// 내용 
-		
-		return successFlag;
-	}
+//	 // insert 
+//	public boolean packageInsert(PackageVO pvo) {
+//		boolean successFlag = false; // 성공여부
+//		Connection con = null;
+//		PreparedStatement pstmt = null;
+//		
+//		try {
+//			con = DBUtility.dbCon();
+//			con.setAutoCommit(false);
+//			
+//			pstmt = con.prepareStatement(PACKAGE_INSERT);
+//			
+//			pstmt.setString(1, pvo.getId());
+//			pstmt.setString(2, pvo.getName());
+//			pstmt.setInt(3, pvo.getPCapacity());
+//			pstmt.setString(4, pvo.getNational());
+//			pstmt.setInt(5, pvo.getPrice());
+//			pstmt.setString(6, pvo.getGuideId());
+//			pstmt.setDate(7, pvo.getSDate());
+//			pstmt.setDate(8, pvo.getEDate());
+//			
+//			// 쿼리 실핼 확인
+//			int result = pstmt.executeUpdate();
+//			
+//			if(result > 0) {
+//				con.commit();
+//				successFlag = true; // 성공시 변경사항 커밋
+//			}else {
+//				con.rollback(); //실패시 롤백 
+//			}
+//		} catch (SQLException e) {
+//			System.out.println("SQL 예외 발생: " + e.getMessage());
+//			try {
+//				if(con != null) {
+//					con.rollback(); // 오류 발생시 롤백 
+//				}
+//			} catch (SQLException sqle) {
+//				sqle.printStackTrace();
+//			}
+//		} finally {
+//			DBUtility.dbClose(con, pstmt);
+//		}
+//		
+//		return successFlag;
+//	}
+//	
 	
+	public boolean packageInsert(PackageVO pvo) {
+	    boolean successFlag = false; // 성공 여부
+	    Connection con = null;
+	    PreparedStatement pstmt = null;
+
+	    try {
+	        con = DBUtility.dbCon();
+	        con.setAutoCommit(false); // 트랜잭션 설정
+
+	        pstmt = con.prepareStatement(PACKAGE_INSERT);
+
+	        pstmt.setString(1, pvo.getId());
+	        pstmt.setString(2, pvo.getName());
+	        pstmt.setInt(3, pvo.getPCapacity());
+	        pstmt.setString(4, pvo.getNational());
+	        pstmt.setInt(5, pvo.getPrice());
+	        pstmt.setString(6, pvo.getGuideId());
+	        pstmt.setDate(7, pvo.getSDate());
+	        pstmt.setDate(8, pvo.getEDate());
+
+	        // 실행
+	        int result = pstmt.executeUpdate();
+
+	        if (result > 0) {
+	            con.commit();
+	            successFlag = true;
+	        } else {
+	            con.rollback();
+	        }
+	    } catch (SQLException e) {
+	        System.out.println("SQL 예외 발생: " + e.getMessage());
+	        try {
+	            if (con != null) con.rollback(); // 롤백
+	        } catch (SQLException rollbackEx) {
+	            rollbackEx.printStackTrace();
+	        }
+	    } finally {
+	        DBUtility.dbClose(con, pstmt);
+	    }
+
+	    return successFlag;
+	}
+
 	
 	
 	
@@ -59,7 +178,36 @@ public class PackageDAO {
 	    Connection con = null;
 	    PreparedStatement pstmt = null;
 	    
-	    // 내용 
+	    try {
+	    	con = DBUtility.dbCon();
+	    	con.setAutoCommit(false);
+	    	pstmt = con.prepareStatement(PACKAGE_UPDATE);
+	    	
+	    	pstmt.setString(1, pvo.getName());
+	    	pstmt.setInt(2, pvo.getPCapacity());
+	    	pstmt.setString(3, pvo.getNational());
+	    	pstmt.setInt(4, pvo.getPrice());
+	    	pstmt.setDate(5, pvo.getSDate());
+	    	pstmt.setDate(6, pvo.getEDate());
+	    	
+	        int result = pstmt.executeUpdate();
+	        if (result > 0) {
+	            con.commit(); // 변경 사항 커밋
+	            successFlag = true;
+	        } else {
+	            con.rollback(); // 실패 시 롤백
+	            System.out.println("업데이트 쿼리 결과: 수정할 정보를 찾을 수 없습니다.");
+	        }
+	    } catch(SQLException e) {
+	        System.out.println("예외 발생: " + e.getMessage());
+	        try {
+	            if (con != null) con.rollback(); // 오류 발생 시 롤백
+	        } catch (SQLException rollbackEx) {
+	            rollbackEx.printStackTrace();
+	        }
+	    } finally {
+	    	DBUtility.dbClose(con, pstmt);
+	    }
 	    
 	    return successFlag;
 	}
@@ -74,7 +222,18 @@ public class PackageDAO {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		
-		// 내용
+		try {
+			con = DBUtility.dbCon();
+			pstmt = con.prepareStatement(PACKAGE_DELETE);
+			pstmt.setInt(1, pvo.getNo());
+			int result = pstmt.executeUpdate();
+			successFlag = (result != 0 ) ? true: false;
+
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		} finally {
+			DBUtility.dbClose(con, pstmt);
+		}
 		
 		return successFlag;
 	}
@@ -90,12 +249,35 @@ public class PackageDAO {
 		
 		ArrayList<PackageVO> packageList = new ArrayList<PackageVO>();
 		
-		// 내용
+		try {
+			con = DBUtility.dbCon();
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(PACKAGE_SORT);
+			
+			while (rs.next()) {
+				int no = rs.getInt("NO");
+				String id = rs.getString("ID");
+				String name = rs.getString("NAME");
+				int pCapacity = rs.getInt("PCAPACITY");
+				String national = rs.getString("NATIONAL");
+				int price = rs.getInt("PRICE");
+				String guideId = rs.getString("GUIDE_ID");
+				Date sDate = rs.getDate("SDATE");
+				Date eDate = rs.getDate("EDATE");
+			
+				PackageVO pvo = new PackageVO(no, id, name, pCapacity, national, price, guideId, sDate, eDate);
+				packageList.add(pvo);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.toString());
+		} finally {
+			DBUtility.dbClose(con, stmt, rs);
+		}
 		
 		return packageList;
 	}
 	
-	// PackageDAO 클래스 내부에 추가
+	// 랜덤생성아이디 중복 확ㅇ니 
 	public boolean isIdDuplicate(String id) {
 	    Connection con = null;
 	    PreparedStatement pstmt = null;
@@ -108,7 +290,7 @@ public class PackageDAO {
 	        con = DBUtility.dbCon();
 
 	        // SQL 쿼리 작성
-	        String sql = "SELECT COUNT(*) FROM PACKAGES WHERE ID = ?";
+	        String sql = "SELECT COUNT(*) FROM PACKAGE WHERE ID = ?";
 
 	        // PreparedStatement에 쿼리 및 파라미터 설정
 	        pstmt = con.prepareStatement(sql);
